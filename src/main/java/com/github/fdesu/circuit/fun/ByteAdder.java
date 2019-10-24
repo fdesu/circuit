2447c01b195454315736382c4004a7f3f517af4e
fdesu <merkel05@gmail.com>
2019-10-24 14:27:36 +0200

    put comments on the 8-bits adder

--- src/main/java/com/github/fdesu/circuit/fun/ByteAdder.java
+++ src/main/java/com/github/fdesu/circuit/fun/ByteAdder.java
@@ -9,15 +9,20 @@ public class ByteAdder {
 
         byte res = 0x0;
         boolean carry = false;
+
+        //flatten the loop to get an estimate on amount of
+        //binary-adders required for the real adder circuit
         for (byte i = 0; i < 8; i++) {
+            //get mask to extract i-th signal
             byte bit = (byte)(1 << i);
 
-            adder.go(
-                    (a & bit) == bit,
-                    (b & bit) == bit,
-                    carry
-            );
+            //parse the i-th signal
+            boolean sig1 = (a & bit) == bit;
+            boolean sig2 = (b & bit) == bit;
 
+            adder.go(sig1, sig2, carry);
+
+            //put the result signal to the i-th position of the result byte
             res |= (byte)((adder.bit() ? 1 : 0) << i);
             carry = adder.carry();
         }
